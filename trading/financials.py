72389e8167a9af855465cc14a1446623663a4dae
Satyam <patelsatyam100@gmail.com>
2023-08-21 16:13:30 -0400

    finished calculating and appending greeks to options chain data

--- trading/financials.py
+++ trading/financials.py
@@ -25,117 +25,160 @@ class OptionsTrading():
         self.ticker_data = ['AAPL']
 
         # Initialize all of the options chain data needed
-        self.options_data = {x: options_chain(x) for x in self.ticker_data}
-
-
-
-
-def calculate_delta_theta(row, risk_free_rate=0.01, dividend_yield=0.0):
-    """
-    Function to calculate the Delta and Theta of a particular option. 
-    This function is calculated on each option through the pd.apply method. 
-
-    The output is then spit out as a new column within the options DataFrame. 
-
-    Delta: Rate of change of the option price relative to the underlying stock price.
-
-    Theta: Time decay of the option contract, with the price decreasing relative to the time to expiration. 
-
-    :param row: A single option and its corresponding feature data  
-    :param risk_free_rate: A constant value that is decided strategically for the delta function. You can choose to alter the constant within 
-                            the function itself. 
-    :param dividend_yield: TBD
-    :return: A pandas Series containing delta and theta which will get merged into the main options chain DataFrame 
-    """
-    S = row['price']  # Underlying asset price
-    K = row['strike'] # Strike price
-    t = row['dte']    # Time to expiration in years
-    sigma = row['impliedVolatility'] # Implied volatility
-    q = dividend_yield # Dividend yield
-
-    # Calculate d1 and d2
-    d1 = (math.log(S / K) + (risk_free_rate - q + (sigma ** 2) / 2) * t) / (sigma * math.sqrt(t))
-    d2 = d1 - sigma * math.sqrt(t)
-
-    # Calculate Delta
-    if row['CALL']:
-        delta = math.exp(-q * t) * norm.cdf(d1)
-    else: # Put option
-        delta = -math.exp(-q * t) * norm.cdf(-d1)
-
-    # Calculate Theta
-    theta_part1 = -(S * sigma * math.exp(-q * t) * norm.pdf(d1)) / (2 * math.sqrt(t))
-    theta_part2 = risk_free_rate * K * math.exp(-risk_free_rate * t) * norm.cdf(d2 if row['CALL'] else -d2)
-    theta_part3 = q * S * math.exp(-q * t) * norm.cdf(d1 if row['CALL'] else -d1)
+        self.options_data = {x: self.options_chain(x) for x in self.ticker_data}
     
-    theta = theta_part1 - theta_part2 + theta_part3 if row['CALL'] else theta_part1 + theta_part2 - theta_part3
+    def print_options(self):
+        print(self.options_data['AAPL'])
 
-    return pd.Series({'delta': delta, 'theta': theta})
-
-
-def options_chain(symbol):
-    tk = yf.Ticker(symbol) 
-    # Expiration dates
-    exps = tk.options
-    options = pd.DataFrame()
-    for e in exps: 
-        opt = tk.option_chain(e)
-        opt = pd.DataFrame()._append(opt.calls)._append(opt.puts)
-        opt['expirationDate'] = e
-        options = options._append(opt, ignore_index=True)
-    
-    # Bizarre error in yfinance gives wrong expiration date
-    # Add 1 day to get the correct expiration date
-    options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days=1) 
-    options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
-
-    # Boolean column if the option is a CALL 
-    options['CALL'] = options['contractSymbol'].str[4:].apply(
-        lambda x: "C" in x)
-
-    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
-    options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate midpoint of the bid-ask
-
-    # Drop unnecessary and meaningless columns
-    options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice'])
-    # Return options chain 
-    return options
+    def print_stocks(self):
+        print(self.stock_data)
 
 
-def on_new_msg(ws, msg):
-    print(msg)
+    def calculate_delta_theta(self, row, risk_free_rate=0.01, dividend_yield=0.0):
+        """
+        Function to calculate the Delta and Theta of a particular option. 
+        This function is calculated on each option through the pd.apply method. 
 
-    global stock_data 
-    timestamp = msg['timestamp']
-    price = msg['price']
-    change_percent = msg['changePercent']
-    day_volume = msg['dayVolume']
+        The output is then spit out as a new column within the options DataFrame. 
 
-    stock_data = stock_data.append({
-        'timestamp': timestamp,
-        'price': price,
-        'changePercent': change_percent,
-        'dayVolume': day_volume,
-    }, ignore_index=True)
+        Delta: Rate of change of the option price relative to the underlying stock price.
+        Theta: Time decay of the option contract, with the price decreasing relative to the time to expiration. 
+
+        :param row: A single option and its corresponding feature data  
+        :param risk_free_rate: A constant value that is decided strategically for the delta function. You can choose to alter the constant within 
+                                the function itself. 
+        :param dividend_yield: TBD
+        
+        :return: A pandas Series containing delta and theta which will get merged into the main options chain DataFrame 
+        """
+        S = row['price']  # Underlying asset price
+        K = row['strike'] # Strike price
+        t = row['dte']    # Time to expiration in years
+        sigma = row['impliedVolatility'] # Implied volatility
+        q = dividend_yield # Dividend yield
+
+        # Calculate d1 and d2
+        d1 = (math.log(S / K) + (risk_free_rate - q + (sigma ** 2) / 2) * t) / (sigma * math.sqrt(t))
+        d2 = d1 - sigma * math.sqrt(t)
+
+        # Calculate Delta
+        if row['CALL']:
+            delta = math.exp(-q * t) * norm.cdf(d1)
+        else: # Put option
+            delta = -math.exp(-q * t) * norm.cdf(-d1)
+
+        # Calculate Theta
+        theta_part1 = -(S * sigma * math.exp(-q * t) * norm.pdf(d1)) / (2 * math.sqrt(t))
+        theta_part2 = risk_free_rate * K * math.exp(-risk_free_rate * t) * norm.cdf(d2 if row['CALL'] else -d2)
+        theta_part3 = q * S * math.exp(-q * t) * norm.cdf(d1 if row['CALL'] else -d1)
+        
+        theta = theta_part1 - theta_part2 + theta_part3 if row['CALL'] else theta_part1 + theta_part2 - theta_part3
+
+        return pd.Series({'delta': delta, 'theta': theta})
 
 
-apple_chain = options_chain('AAPL')
+    def options_chain(self, ticker):
+        """
+        Retrieve the list of options for a particular ticker 
+        """
+        tk = yf.Ticker(ticker) 
+        # Expiration dates
+        exps = tk.options
+        options = pd.DataFrame()
+        for e in exps: 
+            opt = tk.option_chain(e)
+            opt = pd.DataFrame()._append(opt.calls)._append(opt.puts)
+            opt['expirationDate'] = e
+            options = options._append(opt, ignore_index=True)
+        
+        # Bizarre error in yfinance gives wrong expiration date
+        # Add 1 day to get the correct expiration date
+        options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days=1) 
+        options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
 
-# Printing out only the PUT options
-print(apple_chain[apple_chain['CALL'] == False])
+        # Boolean column if the option is a CALL 
+        options['CALL'] = options['contractSymbol'].str[4:].apply(
+            lambda x: "C" in x)
 
-yliveticker.YLiveTicker(on_ticker=on_new_msg, ticker_names=[
-    "AAPL"
-])
+        options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+        options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate midpoint of the bid-ask
 
-# Assuming 'stock_data' is a DataFrame containing the stock information
-# Merge it with the options chain to have the stock price in the same DataFrame
-#merged_data = pd.merge(apple_chain, stock_data, left_on='contractSymbol', right_on='id')
+        # Drop unnecessary and meaningless columns
+        options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice'])
+        # Return options chain 
+        return options
 
-# Apply the function to calculate Delta and Theta for each row
-#options_greeks = merged_data.apply(calculate_delta_theta, axis=1)
 
-# Concatenate the results to the original DataFrame
-#final_options_data = pd.concat([merged_data, options_greeks], axis=1)
+    def on_new_msg(self, ws, msg):
+        """
+        Function to initialize YLiveTicker class with. This function makes use of a websocket pulling real-time 
+        stock data from Yahoo Finance. 
+
+        :param ws: The websocket which is used to pull real-time stock data
+        :param msg: The stock dictionary which is pulled from the websocket. Dictionary contains the
+        following = {'id', 'timestamp', 'price', 'changePercent', 'dayVolume', 'exchange', 'quoteType', 'change', 
+        'priceHint'}
+
+        :return: None, simply appends new stock information into self.stock_data DataFrame and uses updated price 
+        to update options greeks within self.options_data['ticker'] 
+        """
+        print(msg)
+        # Append to the class's stock_data attribute
+        self.stock_data = pd.concat([self.stock_data, pd.DataFrame([{
+            'ticker': msg['id'],
+            'timestamp': msg['timestamp'],
+            'price': msg['price'],
+            'changePercent': msg['changePercent'],
+            'dayVolume': msg['dayVolume'],}])
+            ], ignore_index=True)
+        
+        #print("\n\nStock data...\n")
+        #self.print_stocks()
+        # Calculate delta, theta based on the current stock price
+        self.get_options_with_greeks(msg['id'], msg['price'])
+
+        
+
+    def get_options_with_greeks(self, ticker, price):
+        """
+        Apply the calculate_delta_theta function on each individual options which corresponds to 
+        a particular ticker. 
+
+        This method will ideally be run whenever the underlying ticker price updates. Check the OptionsTrading.on_new_msg method
+        for the full implementation. 
+
+        :param ticker: The ticker in which the deltas and thetas will be calculated for the options chain. 
+
+        :return: 
+        """
+        # Assuming the stock data for the specific ticker is available in self.stock_data
+        #stock_price = self.stock_data[self.stock_data['ticker'] == ticker]['price'].iloc[-1]  # Latest price
+        stock_price = price
+
+        # Get the options chain for the specific ticker
+        options_chain_data = self.options_data[ticker].copy()  # Create a copy to avoid modifying the original DataFrame
+
+        # Add the stock price as a new column
+        options_chain_data['price'] = stock_price
+
+        # Calculate Delta and Theta for each row
+        options_greeks = options_chain_data.apply(self.calculate_delta_theta, axis=1)
+
+        # Concatenate the results to the original DataFrame
+        final_options_data = pd.concat([options_chain_data, options_greeks], axis=1)
+
+        # Update the options data for the specific ticker
+        self.options_data[ticker] = final_options_data
+
+        print("\n\nOptions Data...\n")
+        self.print_options()
+
+        return final_options_data
+
+
+options = OptionsTrading() 
+
+# Initialize websocket and connect with OptionsTrading class 
+yliveticker.YLiveTicker(on_ticker=options.on_new_msg, ticker_names=['AAPL'])
 
 
